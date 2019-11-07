814bc16dfb6feb4d9acaba9f381f56c8216c4620
dvirs12345 <dvirs120@gmail.com>
2019-11-07 12:50:40 +0200

    First commit

---
+++ basicMath.c
@@ -0,0 +1,30 @@
+// Author: Dvir Sadon
+#include <stdio.h>
+float add(float x , float y)
+{
+	return x+y;
+}
+
+float sub(float x , float y)
+{
+	return x-y;
+}
+
+double mul(double x, int y)
+{
+	return y*x;
+}
+
+double div(double x , int y)
+{
+	return x/y;
+}
+int main()
+{
+	//Enter code here ↓↓
+	printf("add = %.4f\n", add(5,3));
+	printf("sub = %.4f\n", sub(5,3));
+	printf("mul = %.4f\n", mul(5,3));
+	printf("div = %.4f\n", div(6,3));
+	return 0;
+}
---
+++ power.c
@@ -0,0 +1,21 @@
+// Author: Dvir Sadon
+#include <stdio.h>
+double exp(int x)
+{
+	
+}
+double pow(double x, int y)
+{
+	int i = 0;
+	double result =1;
+	for(i;i<y;i=i+1)
+	{
+		result*x;
+	} 
+	return result
+}
+int main()
+{
+	//Enter code here ↓↓
+	return 0;
+}

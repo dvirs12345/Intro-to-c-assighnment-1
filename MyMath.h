7938086e02db6dcbf5a4cdb975bad3bce99b982c
noaelram <57635979+noaelram@users.noreply.github.com>
2019-11-12 14:41:37 +0200

    Add files via upload
     h Commit

---
+++ MyMath.h
@@ -0,0 +1,13 @@
+#ifndef _MYMATH_H_
+#define _MYMATH_H_
+
+#define EXP 2.7182
+
+float add(float x, float y);
+float sub(float x, float y);
+double mul(double x, int y);
+double div(double x, int y);
+flaot Exp(int x);
+doble Pow(double x, int y);
+
+#endif // _MYMATH_H_
